1974f0045cb3ef6e9a54c2dac4ae3ca28b5fb973
Koj2584 <73990215+Koj2584@users.noreply.github.com>
2023-11-06 13:05:19 +0100

    Add files via upload

---
+++ Soustavy/Soustavy/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soustavy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = string.Empty;
+            int dec = int.Parse(textBox1.Text);
+            List<byte> list = new List<byte>();
+            while (dec > 0)
+            {
+                list.Add(Convert.ToByte(dec % 2));
+                dec /= 2;
+            }
+            list.Reverse();
+            foreach(byte b in list)
+            {
+                label1.Text += b;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((e.KeyChar<'0'||e.KeyChar>'9')&&e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ Soustavy/Soustavy/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soustavy
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0' || e.KeyChar > '1') && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+                label1.Text = BinToDec(textBox1.Text).ToString();
+            else
+                label1.Text = "-";
+        }
+
+        int BinToDec(string bin)
+        {
+            int vysl = 0;
+            while(bin.Length > 0)
+            {
+                if (bin[0] == '1')
+                {
+                    vysl += Pow(2, bin.Length - 1);
+                }
+                bin = bin.Substring(1);
+            }
+            return vysl;
+        }
+
+        int Pow(int a, int n)
+        {
+            int vysl = 1;
+            for(int i = 0; i < n; i++)
+            {
+                vysl *= a;
+            }
+            return vysl;
+        }
+    }
+}
